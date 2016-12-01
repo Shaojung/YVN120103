1647911897acdad702705886a016b0bcb9f56cd6
shaojung <shaojung@gmail.com>
2016-12-01 21:19:43 +0800

    use onCreateView

--- app/src/main/java/com/example/yvtc/yvn120103/FragmentA.java
+++ app/src/main/java/com/example/yvtc/yvn120103/FragmentA.java
@@ -6,13 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentA extends Fragment {
-
+    TextView tv;
+    Button btn;
 
     public FragmentA() {
         // Required empty public constructor
@@ -23,7 +26,19 @@ public class FragmentA extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a, container, false);
+        View v = inflater.inflate(R.layout.fragment_a, container, false);
+        tv = (TextView) v.findViewById(R.id.textView1);
+        btn = (Button) v.findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("Hello World");
+            }
+        });
+        return v;
     }
+
+
 
 }
