39fde924afb2781ada90bb788c7aec68c68e4fd6
shaojung <shaojung@gmail.com>
2016-12-06 18:59:27 +0800

    use onActivityCreated

--- app/src/main/java/com/example/yvtc/yvn120103/FragmentA.java
+++ app/src/main/java/com/example/yvtc/yvn120103/FragmentA.java
@@ -2,6 +2,7 @@ package com.example.yvtc.yvn120103;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +15,7 @@ import android.widget.TextView;
  * A simple {@link Fragment} subclass.
  */
 public class FragmentA extends Fragment {
-    TextView tv;
+    TextView tv, tv2;
     Button btn;
 
     public FragmentA() {
@@ -30,15 +31,19 @@ public class FragmentA extends Fragment {
         tv = (TextView) v.findViewById(R.id.textView1);
         btn = (Button) v.findViewById(R.id.button);
 
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        tv2 = (TextView) getActivity().findViewById(R.id.textView2);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tv.setText("Hello World");
+                tv2.setText("Hello Hello");
             }
         });
-        return v;
     }
-
-
-
 }
